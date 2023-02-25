db7d95b6034a7cf93b241780e5ddec7f71897a09
Andrew Bwogi <andrew.bwogi@gmail.com>
2023-02-25 14:08:55 -0800

    test routes created

--- server.js
+++ server.js
@@ -7,5 +7,11 @@ connectDB(); //connect to database
 app.get('/', (req, res) => res.send('API Running'));
 app.get('/andrew', (req, res) => res.send('Andrew here...'));
 
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 const port = process.env.PORT || 5001;
 app.listen(port, () => console.log(`Server started on port ${port}`));
