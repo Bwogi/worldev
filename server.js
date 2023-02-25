edfccfc650bf7d43b7b30a86b85fd7117e907535
Andrew Bwogi <andrew.bwogi@gmail.com>
2023-02-25 13:45:28 -0800

    server connected to mongoDB

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
+
+connectDB(); //connect to database
 
 app.get('/', (req, res) => res.send('API Running'));
 app.get('/andrew', (req, res) => res.send('Andrew here...'));
